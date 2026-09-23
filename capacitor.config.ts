099f510be4b365e76b4ca7d936e43ad70c39c665
live-signal29 <mybusiness903@gmail.com>
2026-09-23 07:36:38 +0500

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.median.android.krkqyaz',
+  appName: 'live-signal29',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
