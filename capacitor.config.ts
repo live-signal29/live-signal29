29ca18c7e6ce05ef27a860970ebad5af5d11601d
live-signal29 <mybusiness903@gmail.com>
2026-09-23 07:56:58 +0500

    Update app name in capacitor configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'co.median.android.krkqyaz',
-  appName: 'live-signal29',
+  appName: 'Live Signals Buy/Sell',
   webDir: 'dist',
   bundledWebRuntime: false,
 };
